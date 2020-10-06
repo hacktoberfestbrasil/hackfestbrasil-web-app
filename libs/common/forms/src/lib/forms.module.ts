f286a3807a3c3b4f2b3fd7b771dee321cad7fa7c
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-10-06 07:04:08 -0300

    feat: submit-component

--- libs/common/forms/src/lib/forms.module.ts
+++ libs/common/forms/src/lib/forms.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HackfestSubmitModule } from './submit/submit.module';
 import { HackfestRadioModule } from './radio/radio.module';
 import { HackfestCheckboxModule } from './checkbox/checkbox.module';
 
+export * from './submit/submit.module';
 export * from './radio/radio.module';
 export * from './checkbox/checkbox.module';
 
@@ -10,6 +12,7 @@ export * from './checkbox/checkbox.module';
   exports: [
     HackfestCheckboxModule,
     HackfestRadioModule,
+    HackfestSubmitModule,
     ReactiveFormsModule,
   ],
 })
