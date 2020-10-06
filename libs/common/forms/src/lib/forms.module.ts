95b58d38c6b1c19b0a4c651ae7869286b9d44e1e
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-10-06 07:02:43 -0300

    feat: radio-component

--- libs/common/forms/src/lib/forms.module.ts
+++ libs/common/forms/src/lib/forms.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HackfestRadioModule } from './radio/radio.module';
 import { HackfestCheckboxModule } from './checkbox/checkbox.module';
 
+export * from './radio/radio.module';
 export * from './checkbox/checkbox.module';
 
 @NgModule({
   exports: [
     HackfestCheckboxModule,
+    HackfestRadioModule,
     ReactiveFormsModule,
   ],
 })
