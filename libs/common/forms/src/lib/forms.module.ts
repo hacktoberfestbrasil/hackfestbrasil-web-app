368bf819082ab52eab0a50b166558d2f8057be1b
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-10-06 06:54:05 -0300

    feat: common forms

---
+++ libs/common/forms/src/lib/forms.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  exports: [
+    ReactiveFormsModule,
+  ],
+})
+export class HackfestFormsModule {}
